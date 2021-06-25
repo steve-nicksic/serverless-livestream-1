40eb92333faf103d1a5f4a963ed4958fdc6a432b
steve-nicksic <65583459+steve-nicksic@users.noreply.github.com>
2021-06-25 18:26:56 -0500

    added conditionals

--- hackervoice/index.js
+++ hackervoice/index.js
@@ -7,10 +7,16 @@ module.exports = async function (context, req) {
     //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     var password = req.query.password
+    var response;
+    if (password == "letmein") {
+        response = "Access granted."
+    } else {
+        response = "Access denied."
+    }
 
     context.res = {
         // status: 200, /* Defaults to 200 */
         // body: responseMessage
-        body: password
+        body: response
     };
 }
