dc60faedaf7584350b3d974bad1f0deba11450ac
steve-nicksic <65583459+steve-nicksic@users.noreply.github.com>
2021-06-25 23:55:40 -0500

    finished twocatz

--- twocatz/index.js
+++ twocatz/index.js
@@ -3,19 +3,38 @@ const fetch = require('node-fetch')
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    let resp = await fetch("https://cataas.com/cat/cute/says/Serverless", {
+async function getCat(){
+    let resp = await fetch("https://cataas.com/cat/cute/says/Bitcamp", {
         method: 'GET'
     });
 
     let data = await resp.arrayBuffer()
     
     var base64data = Buffer.from(data).toString('base64')
-//put what you want to turn into base64 inside "originaldata"
-//"originaldata" will be encoded in base64.
 
+    return base64data;
+}
+
+let catpic1 = await getCat()
+let catpic2 = await getCat()
+
+async function getNames() {
+    var names = ["Shreya", "Emily", "Fifi", "Beau", "Evelyn", "Julia", "Daniel", "Fardeen"]
+    var random_value = Math.floor(names.length * Math.random())
+    var resultname = names[random_value]
+
+    return resultname 
+}
+
+let name1 = getNames()
+let name2 = getNames()
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: {base64data}
+        body: {
+            cat1: catpic1,
+            cat2: catpic2,
+            names: [name1, name2]
+        }
     };
 }
