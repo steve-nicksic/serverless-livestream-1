f28d79e962b1f317ba04736b4a39b976fdf45fe9
steve-nicksic <65583459+steve-nicksic@users.noreply.github.com>
2021-07-03 14:03:07 -0500

    find dominate emotion

--- emotionalgif/index.js
+++ emotionalgif/index.js
@@ -16,20 +16,26 @@ module.exports = async function (context, req) {
 
     //analyze the image
     var result = await analyzeImage(imageData);
+
+    let emotions = result[0].faceAttributes.emotion;
+
+    let objects = Object.values(emotions);
+
+    const main_emotion = Object.keys(emotions).find(key => emotions[key] === Math.max(...objects));
+
+
     context.res = {
-        body: {
-            result
-        }
+        body: main_emotion
     };
     console.log(result)
     context.done(); 
-
 
 }
 
 async function analyzeImage(img){
     const subscriptionKey = process.env.SUBSCRIPTIONKEY;
     const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
+
 
     let params = new URLSearchParams({
         'returnFaceId': 'true',
