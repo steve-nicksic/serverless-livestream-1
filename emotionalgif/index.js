0cc2b8904e0572c8db5fe97ecdca5c4dfe4b3a07
steve-nicksic <65583459+steve-nicksic@users.noreply.github.com>
2021-07-03 12:44:52 -0500

    add face api fetch call

--- emotionalgif/index.js
+++ emotionalgif/index.js
@@ -1,4 +1,5 @@
-let multipart = require('parse-multipart');
+var multipart = require('parse-multipart');
+var fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
 
@@ -13,10 +14,41 @@ module.exports = async function (context, req) {
     // save the images raw data in a variable
     var imageData = parts[0].data;
 
-    var convertedResult = Buffer.from(imageData).toString('base64');
-
+    //analyze the image
+    var result = await analyzeImage(imageData);
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: convertedResult
+        body: {
+            result
+        }
     };
+    console.log(result)
+    context.done(); 
+
+
+}
+
+async function analyzeImage(img){
+    const subscriptionKey = process.env.SUBSCRIPTIONKEY;
+    const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
+
+    let params = new URLSearchParams({
+        'returnFaceId': 'true',
+        'returnFaceAttributes': 'emotion'     //FILL IN THIS LINE
+    })
+    
+        //COMPLETE THE CODE
+        let resp = await fetch(uriBase + '?' + params.toString(), {
+            method: 'POST',  //WHAT TYPE OF REQUEST?
+            body: img,  //WHAT ARE WE SENDING TO THE API?
+          
+              //ADD YOUR TWO HEADERS HERE
+            headers: {
+                'Content-Type': 'application/octet-stream',
+                'Ocp-Apim-Subscription-Key': subscriptionKey
+            }
+        })
+
+        let emotionData = await resp.json();
+        
+        return emotionData;
 }
