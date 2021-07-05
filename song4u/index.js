40476c0ea6b19d664d30b898e6c3c43a446bb33c
steve-nicksic <65583459+steve-nicksic@users.noreply.github.com>
2021-07-05 18:48:35 -0500

    add songs to image response

--- song4u/index.js
+++ song4u/index.js
@@ -22,10 +22,23 @@ module.exports = async function (context, req) {
 
     context.log(generation);
 
+    const songs = {"GenZ":"https://open.spotify.com/track/0SIAFU49FFHwR3QnT5Jx0k?si=1c12067c9f2b4fbf", 
+        "GenY":"https://open.spotify.com/track/1Je1IMUlBXcx1Fz0WE7oPT?si=a04bbdf6ec4948b9", 
+        "GenX":"https://open.spotify.com/track/4Zau4QvgyxWiWQ5KQrwL43?si=790d9e3ef2ed408d", 
+        "BabyBoomers":"https://open.spotify.com/track/4gphxUgq0JSFv2BCLhNDiE?si=1abb329f2dc24f50", 
+        "Unknown":"https://open.spotify.com/track/5ygDXis42ncn6kYG14lEVG?si=84b49b41d09d4d11"}
+
+    let song = songs[generation];
+
+    let response = `Sent from your Twilio trial account - We guessed you're part of this generation: ${generation} Happy listening! ${song}`
+
+    
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: generation
+        body: response,
     };
+
+    context.log(response);
 }
 
 function determine_generation(age) {
