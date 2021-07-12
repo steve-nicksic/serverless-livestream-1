45dc6abb43976db9284ef0b532d610b3551abc2b
steve-nicksic <65583459+steve-nicksic@users.noreply.github.com>
2021-07-12 12:36:22 -0500

    updated bunnimage

--- bunnimage-upload/index.js
+++ bunnimage-upload/index.js
@@ -6,10 +6,13 @@ const { BlobServiceClient } = require("@azure/storage-blob");
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
+    var responseMessage = ""
+try {
+    var password = req.headers['codename']; // get the header called "codename"
     var boundary = multipart.getBoundary(req.headers['content-type']);
     var body = req.body;
     var parsedBody = multipart.Parse(body, boundary);
-    
+    // use parse-multipart to parse the body
     var filetype = parsedBody[0].type;
 if (filetype == "image/png") {
     ext = "png";
@@ -21,21 +24,26 @@ if (filetype == "image/png") {
     username = "invalidimage"
     ext = "";
 }
+    // determine the file-type here!
+    var responseMessage = await uploadFile(parsedBody, ext, password);
+} catch(err) {
+    context.log("Undefined body image");
+    responseMessage = "Sorry! No image attached."
+}
 
-    var responseMessage = await uploadFile(parsedBody, ext);
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
     };
 }
 
-async function uploadFile(parsedBody, ext) {
+async function uploadFile(parsedBody, ext, password) {
 
     const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
     const containerName = "images";
     const containerClient = blobServiceClient.getContainerClient(containerName);    // Get a reference to a container
 
-    const blobName = 'test.' + ext;    // Create the container
+    const blobName = password + "." + ext;    // Create the container
     const blockBlobClient = containerClient.getBlockBlobClient(blobName); // Get a block blob client
 
     const uploadBlobResponse = await blockBlobClient.upload(parsedBody[0].data, parsedBody[0].data.length);
