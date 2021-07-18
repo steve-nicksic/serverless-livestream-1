33935b3607fbc40a19cf88f35f322470eb15db5e
steve-nicksic <65583459+steve-nicksic@users.noreply.github.com>
2021-07-18 17:50:52 -0500

    validation

--- bunnimage/index.js
+++ bunnimage/index.js
@@ -1,12 +1,20 @@
-function getImage(){
+function getImage(event){
+    event.preventDefault() //keeps page from reloading/refreshing
 
-    if (document.getElementById("name").value != "") {
+    //to validate that name isn't null:
+    if (document.getElementById("name").value !="") {
+        $('#output').text("Thanks!");
+    } else {
+        alert("No name error.")
+    }
+
+    // if (document.getElementById("name").value != "") {
         // gets value of 'name' id from the html page - 'if the value is not blank...'
 
-        $('#output').text(document.getElementById("name").value + "❤️");
+        // $('#output').text(document.getElementById("name").value + "❤️");
             //jquery - 'get element with the id of output'
             // then 'set the text to value of 'name'(what user inputs) + a heart'
-    }
+    // }
 
     // Other possibilities:
     // jQuery(".output").text(jQuery('#name').val() + "❤️❤️❤️❤️❤️")
