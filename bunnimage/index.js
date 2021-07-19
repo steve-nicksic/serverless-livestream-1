43b9152678ec0faa4311a59fbf7af5ccbe2c08a2
steve-nicksic <65583459+steve-nicksic@users.noreply.github.com>
2021-07-19 08:42:53 -0500

    bunnimage fetch

--- bunnimage/index.js
+++ bunnimage/index.js
@@ -1,9 +1,43 @@
 function getImage(event){
     event.preventDefault() //keeps page from reloading/refreshing
 
+    var bunniForm = document.getElementById("myform");
+    //get image and filename uploaded by user via the form
+    let nameInput = document.getElementById("username");
+    let fileInput = document.getElementById("image");
+
+    var payload = new FormData(bunniForm);
+    console.log(payload);
+
+    const file = fileInput.files[0];
+    payload.append("file", file);
+    $('#output').text("Thanks!");
+    
+
     //to validate that name isn't null:
     if (document.getElementById("name").value !="") {
-        $('#output').text("Thanks!");
+
+        try {
+            let url = "https://baddabing.azurewebsites.net/api/bunnimage-upload?code=pHIOnA9AwG6iv0VfVaSVvFFgTTiUuyp4SqoaCRdwFJ2pyxbtD68LTA=="
+            $('#output').text("Thanks!");
+            console.log("Image was uploaded, making POST request to Azure function")
+
+
+            //Create request to AZURE function
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'codename': nameInput.value
+                },
+                body: payload
+            });
+
+            $('#output').text("Your image has been stored successfully!");
+        } catch (error) {
+            $('#output').text(error)
+        }
+        
+
     } else {
         alert("No name error.")
     }
@@ -20,4 +54,7 @@ function getImage(event){
     // jQuery(".output").text(jQuery('#name').val() + "❤️❤️❤️❤️❤️")
 
     //document.getElementById("output").innerText = document.getElementById("name").value + "❤️"
+
+    //Create request to AZURE function
+    
 }
