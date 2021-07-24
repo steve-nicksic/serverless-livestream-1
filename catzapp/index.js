02bf0b6b7c19954201350d1c4e8a2d78e1ea7c6a
steve-nicksic <65583459+steve-nicksic@users.noreply.github.com>
2021-07-23 21:50:31 -0500

    twocatz 4 names 4 images

--- catzapp/index.js
+++ catzapp/index.js
@@ -1,7 +1,25 @@
 function y1k3s() {
-    let name = document.getElementById("catname").value;
-    let endpoint = "https://cataas.com/cat/says/" + name;
-    if(name != '') {
-        document.getElementById("image").src = endpoint //call to API
+    let name1= document.getElementById("catname1").value;
+    let endpoint1 = "https://cataas.com/cat/says/" + name1;
+    if(name1 != '') {
+        document.getElementById("image1").src = endpoint1 //call to API
+    }
+
+    let name2= document.getElementById("catname2").value;
+    let endpoint2 = "https://cataas.com/cat/says/" + name2;
+    if(name2 != '') {
+        document.getElementById("image2").src = endpoint2 //call to API
+    }
+
+    let name3= document.getElementById("catname3").value;
+    let endpoint3 = "https://cataas.com/cat/says/" + name3;
+    if(name3 != '') {
+        document.getElementById("image3").src = endpoint3 //call to API
+    }
+
+    let name4= document.getElementById("catname4").value;
+    let endpoint4 = "https://cataas.com/cat/says/" + name4;
+    if(name4 != '') {
+        document.getElementById("image4").src = endpoint4 //call to API
     }
 }
